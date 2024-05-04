4e413c8203f5863b6ff9a88282e91ac5e71041ae
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2024-05-04 21:14:16 +0300

    Removed obsolete functions

--- Frontend/lib/bin/data_providers/data_provider.dart
+++ Frontend/lib/bin/data_providers/data_provider.dart
@@ -1,14 +1,8 @@
 import 'package:restaurant_management_app/bin/models/globals_model.dart';
-import 'package:restaurant_management_app/bin/models/product_model.dart';
-import 'package:restaurant_management_app/bin/models/order_model.dart';
 
 // interface for data storage
 // dart does not have interfaces so this is a workaround
 abstract class DataProvider {
-  Future<List<ProductModel>> readProducts();
-  Future<void> writeProducts(List<ProductModel> productList);
-  Future<List<OrderModel>> readOrders();
-  Future<void> writeOrders(List<OrderModel> orderList);
   Future<List<int>> readCapacities();
   Future<void> writeCapacities(List<int> capacities);
   Future<GlobalsModel> readGlobalObject();
--- Frontend/lib/bin/data_providers/json_provider.dart
+++ Frontend/lib/bin/data_providers/json_provider.dart
@@ -3,9 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/services.dart' as services;
 import 'package:restaurant_management_app/bin/models/globals_model.dart';
-import 'package:restaurant_management_app/bin/models/product_model.dart';
 import 'package:restaurant_management_app/bin/data_providers/data_provider.dart';
-import 'package:restaurant_management_app/bin/models/order_model.dart';
 
 class JsonProvider implements DataProvider {
   static const jsonPath = "assets/json/";
@@ -26,52 +24,26 @@ class JsonProvider implements DataProvider {
   JsonProvider();
 
   @override
-  Future<List<ProductModel>> readProducts() async {
-    final jsondata = await services.rootBundle.loadString(productPath);
-    final data = json.decode(jsondata) as List<dynamic>;
-    return data.map((x) => ProductModel.fromJson(x)).toList();
-  }
-
-  @override
-  Future<void> writeProducts(List<ProductModel> productList) async {
-    String productString = "[${productList.join(",\n")}]";
-    await File(productPath).writeAsString(productString);
-  }
-  
-  @override
-  Future<List<OrderModel>> readOrders() async {
-    final jsondata = await services.rootBundle.loadString(orderPath);
-    final data = json.decode(jsondata) as List<dynamic>;
-    return data.map((x) => OrderModel.fromJson(x)).toList();
-  }
-
-  @override
-  Future<void> writeOrders(List<OrderModel> orderList) async {
-    String orderString = "[${orderList.join(",\n")}]";
-    await File(orderPath).writeAsString(orderString);
-  }
-
-  @override
-  Future<List<int>> readCapacities() async{
+  Future<List<int>> readCapacities() async {
     final jsondata = await services.rootBundle.loadString(capacitiesPath);
     final data = json.decode(jsondata) as List<dynamic>;
     return data.cast<int>();
   }
 
   @override
-  Future<void> writeCapacities(List<int> capacities) async{
+  Future<void> writeCapacities(List<int> capacities) async {
     await File(capacitiesPath).writeAsString(json.encode(capacities));
   }
 
   @override
-  Future<GlobalsModel> readGlobalObject() async{
+  Future<GlobalsModel> readGlobalObject() async {
     final jsondata = await services.rootBundle.loadString(globalsPath);
     final data = json.decode(jsondata) as dynamic;
     return GlobalsModel.fromJson(data);
   }
 
   @override
-  Future<void> writeGlobalObject(GlobalsModel globals) async{
+  Future<void> writeGlobalObject(GlobalsModel globals) async {
     String globalsString = globals.toString();
     await File(globalsPath).writeAsString(globalsString);
   }
