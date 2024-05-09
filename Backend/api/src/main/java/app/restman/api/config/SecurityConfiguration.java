2bcdf0b338fe4a94a62337611224acaab36cee1a
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2024-05-09 18:55:17 +0300

    Added JWT Bearer to swagger

--- Backend/api/src/main/java/app/restman/api/config/SecurityConfiguration.java
+++ Backend/api/src/main/java/app/restman/api/config/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package app.restman.api.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -62,5 +66,19 @@ public class SecurityConfiguration {
         source.registerCorsConfiguration("/**",configuration);
 
         return source;
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()));
     }
 }
