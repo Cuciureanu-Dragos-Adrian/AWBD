ec0fbe8b8cd99f8770e2474284052a16f3e8ab9d
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2024-06-16 15:03:37 +0300

    Add non-functional gateway server

---
+++ Gateway-server/src/main/java/app/restman/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,15 @@
+package app.restman.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+}
